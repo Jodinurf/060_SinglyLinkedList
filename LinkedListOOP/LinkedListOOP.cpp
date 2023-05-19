a4aa9993bb697c6d4a816732cfe1c3560bfac777
Jodinurf <jodinurfarkhani04@gmail.com>
2023-05-19 17:38:00 +0700

    membuat kendali program while dimana selama current tidak sama dengan kosong dan nim tidak sama dengan nilai noMhs pada current maka
    posisi previous akan mengikuti current, dan posisi current akan berpindah ke setelah current.
    
    setelah loop diatas maka current akan dikembalikan ke kosong

--- LinkedListOOP/LinkedListOOP.cpp
+++ LinkedListOOP/LinkedListOOP.cpp
@@ -97,7 +97,12 @@ bool List::Search(int nim, Node * *previous, Node * *current)
 {
 	*previous = START;
 	*current = START;
-
+	while ((*current != NULL) && (nim != (*current)->noMhs))
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
 }
 
 
